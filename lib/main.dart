7404685fb4e4dd131cdd7813ddda5e29816c5b67
ato-codes <thecmboy930@gmail.com>
2023-05-18 01:25:13 -0400

    adding layout

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,24 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      'What is Youre favorite color?',
+      'What is your favorite animal',
+    ];
     return MaterialApp(
         home: Scaffold(
       // Scaffold Creates a basic window fit for device
       appBar: AppBar(
         title: Text("My App Title"),
       ),
-      body: Text("Hello AM Ato-codes"),
+      body: const Column(
+        children: [
+          Text("The Question"),
+          ElevatedButton(child: Text('Answer 1'), onPressed: null),
+          ElevatedButton(child: Text('Answer 2'), onPressed: null),
+          ElevatedButton(child: Text('Answer 3'), onPressed: null),
+        ],
+      ),
     ));
   }
 }
