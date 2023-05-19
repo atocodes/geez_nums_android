9fbb74c098afef42a79bcf3c705dc80b24847732
ato-codes <thecmboy930@gmail.com>
2023-05-19 00:30:11 -0400

    passing callback functions through classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 void main() => runApp(MyApp());
 
@@ -45,13 +46,9 @@ class _MyAppState extends State<MyApp> {
       body: Column(
         children: [
           Question(questions[_questionIndex]),
-          ElevatedButton(child: Text('Answer 1'), onPressed: _answerQuestion),
-          ElevatedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print('answer 2 Chosen')),
-          ElevatedButton(
-              child: Text('Answer 3'),
-              onPressed: () => print('Answer 3 Chosen')),
+          Answer(_answerQuestion),
+          Answer(_answerQuestion),
+          Answer(_answerQuestion),
         ],
       ),
     ));
